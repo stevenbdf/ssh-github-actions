e6685fcc0fe1e90bd4aee96f4b4240f22644a5e3
Steven Diaz <stevenbdf@gmail.com>
2021-04-27 11:19:25 -0600

    New version button 1

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import './button.css';
 
 /**
- * Primary UI component for user interaction
+ * Primary UI component for user interaction, new version
  */
 const Button = ({ primary, backgroundColor, size, label, ...props }) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
